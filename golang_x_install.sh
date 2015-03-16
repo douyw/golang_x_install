5c824240ddbe7b9a4b8ffe98953c3b323a03f477
Yongwang Dou <douyongwang@gmail.com>
2015-03-16 09:33:44 +0800

    add files

---
+++ golang_x_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -d $GOPATH ]; then
+  echo "GOPATH is net set. exit"
+  exit
+fi
+
+GOXPATH=$GOPATH/src/golang.org/x
+
+if [ ! $GOXPATH]; then
+  mkdir -p $GOXPATH
+fi
+
+cd $GOXPATH
+
+for mod in net oauth2 sys tools tour
+do
+  if [ -d $mod ]; then
+    cd $mod && echo "pull $mod..." && git pull && cd ..
+  else
+    git clone https://github.com/golang/$mod.git
+  fi
+done
